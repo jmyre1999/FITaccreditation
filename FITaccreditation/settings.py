a8bfde93e97bdab024bfd04bf2d2f125b29f62ba
jmyre1999 <jwilliammyre@gmail.com>
2020-09-18 12:04:13 -0400

    all hosts allowed

--- FITaccreditation/settings.py
+++ FITaccreditation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.24', '107.145.127.195']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
