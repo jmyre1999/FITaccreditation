9ea5a69626187e7bb3059d3437937fa178bfe154
jmyre1999 <jwilliammyre@gmail.com>
2020-11-06 20:14:42 -0500

    temp removed static url

--- FITaccreditation/urls.py
+++ FITaccreditation/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
