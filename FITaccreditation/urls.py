ec17740b009852f209b481e4016bb8a0bf972cc4
Justynnn <53898958+Justynnn@users.noreply.github.com>
2021-04-09 10:49:13 -0400

    Fixed submission page formatting.

--- FITaccreditation/urls.py
+++ FITaccreditation/urls.py
@@ -36,4 +36,4 @@ urlpatterns = [
         name="password_reset_complete"),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
